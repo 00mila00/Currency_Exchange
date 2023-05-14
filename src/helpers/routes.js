357826128134281b562664ea967ad9ffce963ae7
Mila <93038886+00mila00@users.noreply.github.com>
2023-05-14 21:48:16 +0200

    Update routes.js

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,6 +1,5 @@
 const serverUrl = 'http://api.nbp.pl/api/exchangerates/tables'
 const currUrl = 'http://api.nbp.pl/api/exchangerates/rates/A/'
-const serverGoldUrl = 'http://api.nbp.pl/api/cenyzlota'
 
 export const currencyUrl = code =>
 code ? `${currUrl}/${code}/` : `${serverUrl}/A/`
